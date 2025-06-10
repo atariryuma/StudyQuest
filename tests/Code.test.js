ad78e148bd5cf662e30b377f52896a7e13f23977
Atari Ryuma <126367011+atariryuma@users.noreply.github.com>
2025-06-10 10:38:53 +0900

    refactor: extract CSV template helpers

--- tests/Code.test.js
+++ tests/Code.test.js
@@ -1,5 +1,10 @@
 const { getSqVersion } = require('../src/Code.gs');
+const { getStudentTemplateCsv } = require('../src/StudentCsv.gs');
 
 test('getSqVersion returns correct version', () => {
   expect(getSqVersion()).toBe('v1.0.191');
+});
+
+test('getStudentTemplateCsv returns header row', () => {
+  expect(getStudentTemplateCsv()).toBe('Email,Name,Grade,Class,Number\n');
 });
